4d9fa259132823249d5a779c702e7491d1d10e1b
UlybAV <PULTYUMEN@mail.ru>
2022-05-20 11:30:44 +0500

    My wrong way

--- jsprog.js
+++ jsprog.js
@@ -1,1 +1,30 @@
 console.log('start');
+
+function work (a, b) {
+  console.log(a + b); // произвольная функция или метод
+}
+
+function spy (func) {
+  const cache = [];
+  return function (...args) {
+    cache.push([args[0], args[1]]);
+    console.log('args = ', args);
+    console.log('cache = ', cache);
+    console.log('func = ', func);
+    const result = func(args[0], args[1]);
+    return result;
+  };
+}
+
+work = spy(work);
+
+work(1, 2); // 3
+work(4, 5); // 9
+
+console.log(work.cache);
+
+/*
+for (let args of work.calls) {
+  console.log( 'call:' + args.join() ); // "call:1,2", "call:4,5"
+}
+*/
