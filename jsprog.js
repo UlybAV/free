54f7e3452c3f1e0078c5d9edead8d6748a57d79e
UlybAV <PULTYUMEN@mail.ru>
2022-05-20 11:58:52 +0500

    Доработано. Finalized.

--- jsprog.js
+++ jsprog.js
@@ -1,12 +1,12 @@
 console.log('start');
 
 function work (a, b) {
-  console.log(a + b); // произвольная функция или метод
+  console.log('Func result = ', a + b); // произвольная функция или метод
 }
 
 function spy (func) {
-  function wrap(...args) {
-    wrap.cache.push([args[0], args[1]]);
+  function wrap (...args) {
+    wrap.cache.push(args);
     console.log('args = ', args);
     console.log('cache = ', wrap.cache);
     console.log('func = ', func);
@@ -21,11 +21,10 @@ function spy (func) {
 work = spy(work);
 
 work(1, 2); // 3
-work(4, 5); // 9
+work(4, 5, 9, 10); // 9
 
 console.log(work.cache);
 
-
-for (let args of work.cache) {
-  console.log( 'call:' + args.join() ); // "call:1,2", "call:4,5"
+for (const args of work.cache) {
+  console.log('call:' + args.join()); // "call:1,2", "call:4,5"
 }
